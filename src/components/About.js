02d09eaeee4e3eb1212872b206588155a341b086
PBIndustries Support <pbindustriesapps@gmail.com>
2019-07-09 22:54:25 -0400

    fixed description

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,7 @@ const About = () => (
       <div className="nine columns main-col">
         <h2>About Me</h2>
         <p>
-          Motivated Software Engineer with solid technical skills enabling me to contribute to your organization's projects by solving its challenges. By developing according to best practices I ensure maximum reliability, reusability and overall code integrity. Recently I've been specializing in Elastic Stack development and intend to be certified in Elasticsearch by November 2018.
+          Motivated Software/DevOps Engineer with solid technical skills enabling me to contribute to your organization's projects by solving its challenges. By developing according to best practices I ensure maximum reliability, reusability and overall code integrity.
           <br/>
           Checkout my Projects at:
           <br/>
